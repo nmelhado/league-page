f5c7561bb6e02b19c8fcffa52a0946f009d29d70
Nicholas Melhado <nmelhado@freewheel.tv>
2021-08-01 01:37:10 -0400

    cleanup repo

--- node_modules/@sveltejs/adapter-vercel/index.d.ts
+++
@@ -1,6 +0,0 @@
-type BuildOptions = import('esbuild').BuildOptions;
-declare function plugin(options?: {
-	esbuild?: (defaultOptions: BuildOptions) => Promise<BuildOptions> | BuildOptions;
-}): import('@sveltejs/kit').Adapter;
-
-export = plugin;
--- node_modules/@sveltejs/kit/dist/chunks/_commonjsHelpers.js
+++
@@ -1,8 +0,0 @@
-var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
-
-function createCommonjsModule(fn) {
-  var module = { exports: {} };
-	return fn(module, module.exports), module.exports;
-}
-
-export { commonjsGlobal as a, createCommonjsModule as c };
