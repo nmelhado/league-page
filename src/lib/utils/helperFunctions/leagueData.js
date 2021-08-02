c97064cac561b940d90f5085a09a238e00dfc78b
xnickedge <83649070+xnickedge@users.noreply.github.com>
2021-08-02 08:32:20 -0400

    Update leagueData.js

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -1,8 +1,8 @@
 import { get } from 'svelte/store';
 import {leagueData} from '$lib/stores';
 
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "650175281549635584"; // your league ID
+export const leagueName = "Backyard Brawl"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
