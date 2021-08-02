ce4b2073ca523a7e8eab5d8305ff195dca9dbfcd
xnickedge <83649070+xnickedge@users.noreply.github.com>
2021-08-02 09:13:28 -0400

    Update managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -1,16 +1,16 @@
 export const managers = [
   // {
   //   "roster": 1,  // ID of the roster that the manager manages
-  //   "name": "Your Name", // (optional) used if a manager took over a team, null or omit otherwise
-  //   "location": "Brooklyn",
-  //   "bio": "Lorem ipsum...",
+  //   "name": "Jeremy", // (optional) used if a manager took over a team, null or omit otherwise
+  //   "location": "Ohio",
+  //   "bio": "Made my riches robbing Brandon blind",
   //   "photo": "/managers/name.jpg", // done
-  //   "fantasyStart": 2014, // when did the manager start playing fantasy football
-  //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+  //   "fantasyStart": 1954, // when did the manager start playing fantasy football
+  //   "favoriteTeam": "cle", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
   //   "mode": "Win Now", // 'Win Now' or 'Rebuild'
   //   "rival": {
-  //     name: "Rival", // Can be anything (usually your rival's name)
-  //     link: 6, // manager array number within this array, or null to link back to all managers page
+  //     name: "Nick", // Can be anything (usually your rival's name)
+  //     link: 1, // manager array number within this array, or null to link back to all managers page
   //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
   //   },
   //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
