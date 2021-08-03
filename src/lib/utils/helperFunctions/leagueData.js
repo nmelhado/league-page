d7e837ccb1d975551a1e505482d114b2d7786f41
jameslesliewatts <jameslesliewatts@gmail.com>
2021-08-02 22:20:45 -0500

    Initial league site setup

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -1,10 +1,10 @@
 import { get } from 'svelte/store';
 import {leagueData} from '$lib/stores';
 
-export const leagueID = "650025991288205312"; // your league ID
-export const leagueName = "your_league_name"; // your league name
-export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const leagueID = "728116681330794496"; // your league ID
+export const leagueName = "[foot]BALL IS LIFE!"; // your league name
+export const dues = 75; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 
 export const getLeagueData = async (queryLeagueID = leagueID) => {
 	if(get(leagueData)[queryLeagueID]) {
