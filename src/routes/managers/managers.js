cc6a6d018f87b120be63b70519ee10d5c20d0aa3
Nicholas Melhado <nmelhado@freewheel.tv>
2021-08-04 09:47:17 -0400

    add email and phone options and better comments to managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -11,32 +11,32 @@ export const managers = [
   //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   //   "location": "Brooklyn",
   //   "bio": "Lorem ipsum...",
-  //   "photo": "/managers/name.jpg", // done
+  //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   //   "fantasyStart": 2014, // when did the manager start playing fantasy football
   //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-  //   "mode": "Win Now", // 'Win Now' or 'Rebuild'
+  //   "mode": "Win Now", // 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   //   "rival": {
   //     name: "Rival", // Can be anything (usually your rival's name)
   //     link: 6, // manager array number within this array, or null to link back to all managers page
-  //     image: "/managers/rival.jpg", // either a specific manager photo or 'everyone' or 'question'
+  //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
   //   },
   //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
   //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
-  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
   //   "philosophy": "Your fantasy team's philosophy",
   //   "tradingScale": 10, // 1 - 10
-  //   "preferredContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
   // },
   // {
   //   "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   //   "name": "Your Name",
-  //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+  //   "tookOver": 2020, // (optional) used if a manager took over a team, set to null otherwise
   //   "location": "Brooklyn",
   //   "bio": "Lorem ipsum...",
-  //   "photo": "/managers/name.jpg", // done
+  //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   //   "fantasyStart": 2014, // when did the manager start playing fantasy football
   //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-  //   "mode": "Win Now", // 'Win Now' or 'Rebuild'
+  //   "mode": "Win Now", // 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   //   "rival": {
   //     name: "Rival", // Can be anything (usually your rival's name)
   //     link: 6, // manager array number within this array, or null to link back to all managers page
@@ -44,10 +44,10 @@ export const managers = [
   //   },
   //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
   //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
-  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
   //   "philosophy": "Your fantasy team's philosophy",
   //   "tradingScale": 10, // 1 - 10
-  //   "preferredContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
   // },
   // {
   //   "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
@@ -55,10 +55,10 @@ export const managers = [
   //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
   //   "location": "Brooklyn",
   //   "bio": "Lorem ipsum...",
-  //   "photo": "/managers/name.jpg", // done
+  //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
   //   "fantasyStart": 2014, // when did the manager start playing fantasy football
   //   "favoriteTeam": "nyj", // favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-  //   "mode": "Win Now", // 'Win Now' or 'Rebuild'
+  //   "mode": "Win Now", // 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
   //   "rival": {
   //     name: "Rival", // Can be anything (usually your rival's name)
   //     link: 6, // manager array number within this array, or null to link back to all managers page
@@ -66,9 +66,9 @@ export const managers = [
   //   },
   //   "favoritePlayer": 1426, // this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
   //   "valuePosition": "WR", // Favorite position (QB, WR, RB, TE, etc.)
-  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets'
+  //   "rookieOrVets": "Rookies", // 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
   //   "philosophy": "Your fantasy team's philosophy",
   //   "tradingScale": 10, // 1 - 10
-  //   "preferredContact": "Text", // 'Text', 'WhatsApp', and 'Carrier Pigeon' are currently supplied in the template
+  //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
   // },
 ]
