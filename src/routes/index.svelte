0777151b77f43ac5cb351a637e03c747db13d073
thedoctor692 <88405218+thedoctor692@users.noreply.github.com>
2021-08-03 19:57:44 -0600

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -155,12 +155,8 @@
     <div id="main">
         <div class="text">
             <h6>{leagueName}</h6>
-            <p>This is a sweet fantasy league page that Fraser is going to put somet stuff here about the history.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>This is a sweet fantasy league page intro. Fraser is going to put some stuff here about the history and such.</p>
+            <p>Power Rankings will be displayed below after the draft and will auto-update!</p>
         </div>
         <PowerRankings />
     </div>
