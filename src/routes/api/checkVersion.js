f3d17f7ab24dc374411cf5ee30bb9a1b6d4fefb9
Nicholas Melhado <n.melhado@gmail.com>
2021-08-16 22:51:19 -0400

    remove extra console.log/test update functionality

--- src/routes/api/checkVersion.js
+++ src/routes/api/checkVersion.js
@@ -6,7 +6,6 @@ export async function get() {
     if(res.ok) {
         const globalVersion = await res.json();
         console.log(`VERSION CHECK: global version: ${globalVersion}, local version: ${version}`);
-        console.log(version);
         // If it reaches the global checkpoint and the versions do not match
         // set needsUpdate to true to display the update prompt
         needsUpdate = globalVersion != version;
