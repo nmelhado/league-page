85cab0d6808a06f2c983a32fe8de2d2e00d7e797
thedoctor692 <88405218+thedoctor692@users.noreply.github.com>
2021-09-15 16:19:30 -0600

    Update PowerRankingsDisplay.svelte

--- src/lib/PowerRankings/PowerRankingsDisplay.svelte
+++ src/lib/PowerRankings/PowerRankingsDisplay.svelte
@@ -60,7 +60,7 @@
 
         // Fraser, Sam, Ryan, Matt, Brandon, Trey, Erik, Kyle, Ward Connor, Elliot Foster
         // rosterPower.powerScore = {89, 64, 74, 100, 76, 68, 71, 81, 54, 82, 83, 71};  // draft
-        manualScores = [82, 71, 76, 100, 74, 80, 76, 79, 62, 89, 84, 73];  // week 1
+        // manualScores = [82, 71, 76, 100, 74, 80, 76, 79, 62, 89, 84, 73];  // week 1
         for(const rosterPower of rosterPowers) {
             rosterPower.powerScore = 100;
         }
