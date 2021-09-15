e0425895d7ad5b66a18242afcc1f10ec2b3f218c
thedoctor692 <88405218+thedoctor692@users.noreply.github.com>
2021-09-15 16:17:47 -0600

    Update PowerRankingsDisplay.svelte

--- src/lib/PowerRankings/PowerRankingsDisplay.svelte
+++ src/lib/PowerRankings/PowerRankingsDisplay.svelte
@@ -49,12 +49,12 @@
                 powerScore: 0,
             }
             const seasonEnd = Object.keys(rosterPlayers[0].weeklyInfo).length || 18;
-            /*for(let i = week; i < seasonEnd; i++) {
+            for(let i = week; i < seasonEnd; i++) {
                 rosterPower.powerScore += predictScores(rosterPlayers, i, leagueData);
             }
             if(rosterPower.powerScore > max) {
                 max = rosterPower.powerScore;
-            }*/
+            }
             rosterPowers.push(rosterPower);
         }
 
