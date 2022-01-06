aa156bac119ddfbd0c92ca7c99156e8a36372d03
OldNewsBlues-2 <91703566+OldNewsBlues-2@users.noreply.github.com>
2022-01-06 11:20:36 -0600

    Adds Tables for Lowest Week/Season Scores #74 (#93)
    * commit on not-top-ten-recordtables
    
    * remove double sort and change sort logic to PPG
    
    Co-authored-by: OldNewsBlues <kovacsjc@gmail.com>
    Co-authored-by: Nicholas Melhado <n.melhado@gmail.com>

--- src/lib/Records/index.svelte
+++ src/lib/Records/index.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Button, { Group, Label } from '@smui/button';
     import { getLeagueRecords, getLeagueTransactions } from '$lib/utils/helper';
 
     import AllTimeRecords from './AllTimeRecords.svelte';
@@ -11,7 +12,7 @@
         totals = newTransactions.totals;
     }
 
-    let {leagueRosterRecords, leagueWeekRecords, allTimeClosestMatchups, allTimeBiggestBlowouts, currentManagers, mostSeasonLongPoints, seasonWeekRecords, currentYear, lastYear} = leagueRecords;
+    let {leagueRosterRecords, leagueWeekRecords, leagueWeekLows, allTimeClosestMatchups, allTimeBiggestBlowouts, currentManagers, mostSeasonLongPoints, leastSeasonLongPoints, seasonWeekRecords, currentYear, lastYear} = leagueRecords;
 
     const refreshRecords = async () => {
         const newRecords = await getLeagueRecords(true);
@@ -20,10 +21,12 @@
         leagueRecords = newRecords;
         leagueRosterRecords = newRecords.leagueRosterRecords;
         leagueWeekRecords = newRecords.leagueWeekRecords;
+        leagueWeekLows = newRecords.leagueWeekLows;
         allTimeClosestMatchups = newRecords.allTimeClosestMatchups;
         allTimeBiggestBlowouts = newRecords.allTimeBiggestBlowouts;
         currentManagers = newRecords.currentManagers;
         mostSeasonLongPoints = newRecords.mostSeasonLongPoints;
+        leastSeasonLongPoints = newRecords.leastSeasonLongPoints;
         seasonWeekRecords = newRecords.seasonWeekRecords;
         currentYear = newRecords.currentYear;
         lastYear = newRecords.lastYear;
@@ -36,6 +39,8 @@
     if(leagueRecords.stale) {
         refreshRecords();
     }
+
+    let display = "allTime"
 
 </script>
 
@@ -50,13 +55,58 @@
         margin: 10em 0 4em;
         text-align: center;
     }
+
+    /* Button Styling */
+    .buttonHolder {
+        text-align: center;
+        margin: 2em 0 0;
+    }
+
+    /* Start button resizing */
+
+    @media (max-width: 540px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.6em;
+        }
+    }
+
+    @media (max-width: 415px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.5em;
+            padding: 0 6px;
+        }
+    }
+
+    @media (max-width: 315px) {
+        :global(.buttonHolder .selectionButtons) {
+            font-size: 0.45em;
+            padding: 0 3px;
+        }
+    }
+
+    /* End button resizing */
 </style>
 
 <div class="rankingsWrapper">
-    {#if leagueWeekRecords.length}
-        <AllTimeRecords transactionTotals={totals} {allTimeClosestMatchups} {allTimeBiggestBlowouts} {leagueRosterRecords} {leagueWeekRecords} {currentManagers} {mostSeasonLongPoints} />
+
+    <div class="buttonHolder">
+        <Group variant="outlined">
+            <Button class="selectionButtons" on:click={() => display = "allTime"} variant="{display == "allTime" ? "raised" : "outlined"}">
+                <Label>All-Time Records</Label>
+            </Button>
+            <Button class="selectionButtons" on:click={() => display = "season"} variant="{display == "season" ? "raised" : "outlined"}">
+                <Label>Season Records</Label>
+            </Button>
+        </Group>
+    </div>
+
+    {#if display == "allTime"}
+        {#if leagueWeekRecords.length}
+            <AllTimeRecords transactionTotals={totals} {allTimeClosestMatchups} {allTimeBiggestBlowouts} {leagueRosterRecords} {leagueWeekRecords} {leagueWeekLows} {currentManagers} {mostSeasonLongPoints} {leastSeasonLongPoints} />
+        {:else}
+            <p class="empty">No records <i>yet</i>...</p>
+        {/if}
     {:else}
-        <p class="empty">No records <i>yet</i>...</p>
+        <PerSeasonRecords transactionTotals={totals} {leagueRosterRecords} {seasonWeekRecords} {currentManagers} {currentYear} {lastYear} />
     {/if}
-    <PerSeasonRecords transactionTotals={totals} {leagueRosterRecords} {seasonWeekRecords} {currentManagers} {currentYear} {lastYear} />
 </div>
