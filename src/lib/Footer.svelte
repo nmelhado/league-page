51b23466d2bef121c36c4d52ed516cb7be181659
Pnysch97 <99479390+Pnysch97@users.noreply.github.com>
2022-02-11 09:35:19 -0500

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -104,13 +104,15 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
+	<span class="copyright">&copy; 2020 - {year} 
+		<!-- <a href="https://github.com/nmelhado/league-page">League Page</a> -->
+	</span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<!-- <span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span> -->
 	<!-- You can remove the donation link (although any donations to help
 	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+	<!-- Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you! -->
 </footer>
 
 <footer class="{!outOfDate ? '' : 'invisible'}" bind:this={el}>
