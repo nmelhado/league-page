fd6ed5ee607e1a52606f820b6fd6e2800bd585eb
Pnysch97 <99479390+Pnysch97@users.noreply.github.com>
2022-02-15 19:34:53 -0500

    jpg -> png

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -183,7 +183,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Blue Bell, PA", // (optional)
       "bio": "...",
-      "photo": "/managers/peter.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/peter.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
