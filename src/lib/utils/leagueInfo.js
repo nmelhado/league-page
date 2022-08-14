5c5085f83f338ba1145d2cf16214fc86446bf87b
JuicedFrank <111188823+JuicedFrank@users.noreply.github.com>
2022-08-14 11:29:58 -0400

    MGR Update

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,7 +27,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     // {
     //   "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
+    //   "name": "Stephen",
     //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
     //   "location": "Brooklyn", // (optional)
     //   "bio": "Lorem ipsum...",
