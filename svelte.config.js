cb3ab21dd26b7352ab5d9905f48d86fa6ddd6be0
Ash Coca <ashvincoca@gmail.com>
2022-10-05 18:06:57 -0700

    Creating adapter for amplify

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,27 @@
-import vercel from '@sveltejs/adapter-vercel';
-import node from '@sveltejs/adapter-node';
+// import vercel from '@sveltejs/adapter-vercel';
+// import node from '@sveltejs/adapter-node';
 
-const dockerBuild = process.env.DOCKER_BUILD
+// const dockerBuild = process.env.DOCKER_BUILD
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: dockerBuild ? node() : vercel(),
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: dockerBuild ? node() : vercel(),
-	}
+    kit: {
+        adapter: adapter(),
+        prerender: {
+            default: true
+        }
+    }
 };
 
 export default config;
