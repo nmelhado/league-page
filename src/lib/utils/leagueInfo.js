eb9a1f16ce87d553876b1c8a447df098a2f8df68
Tom Bellis <68870074+sillebt@users.noreply.github.com>
2023-04-08 15:20:10 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,10 +8,10 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>This is the homepage of the exclusive Dynasty Fantasy Insanity Experience. Welcome to New Guy, Pat! Look forward to a competitive roster in 2026! #AnyoneButM____e</p>
-  <p><strong>NOTICE - DRAFT IN VEGAS - VIVA LAS VEGAS - BE IN LAS VEGAS<strong></p>
+  <p>NOTICE - DRAFT IN VEGAS - VIVA LAS VEGAS - BE IN LAS VEGAS</p>
   <p><em><a href="https://pbs.twimg.com/media/FnsRwmeaYAA6KCS?format=jpg&name=large">Site Under Construction</a></em></p>
   <p>Champions: Tommy* (2022), Naad (2021), Hosta (2020) </p>
-  <p><strong>League History<strong></p>
+  <p>League History</p>
   <p>2022-23: League Champ - Tommy* | Runner-Up - Tom<br>Regular Season Best Record - Hosta (11-3) | Worst Record - Conor (1-13)<br>Regular Season Most Points - Tom (1,967) | Least Points - Logan (1,383)</p>
   <p>2021-22: League Champ - Naad | Runner-Up - Tom<br>Regular Season Best Record - Zac (10-4) | Worst Record - Conor (3-11)<br>Regular Season Most Points - Tom (1,995) | Least Points - Conor (1,503)</p>
   <p>2021-22: League Champ - Hosta | Runner-Up - Tom<br>Regular Season Best Record - Hosta (9-4) | Worst Record - Montel + Aviel (3-11, Tied)<br>Regular Season Most Points - Hosta (1,885) | Least Points - Monty (1,382)</p>
