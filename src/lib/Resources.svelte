eeab341a72a6f0b0f59feef44935b1d4ff383f2c
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 10:01:12 -0400

    added keep trade cut

--- src/lib/Resources.svelte
+++ src/lib/Resources.svelte
@@ -1,95 +1,116 @@
 <script>
-    import List, { Item, Graphic, Separator, Text } from '@smui/list';
-import { dynasty } from './utils/helper';
+	import List, { Item, Graphic, Separator, Text } from '@smui/list';
+	import { dynasty } from './utils/helper';
 
-    const today = new Date();
-    const resources = [
-        {
-            name: "FTN Fantasy",
-            url: "https://www.ftnfantasy.com/nfl",
-            icon: "insights",
-            premium: true,
-            separator: true,
-        },
-        {
-            name: "The Fantasy Footballers",
-            url: "https://www.thefantasyfootballers.com/",
-            icon: "podcasts",
-            premium: true,
-            separator: true,
-        },
-        {
-            name: "FantasyPros Dynasty Trade Value Chart",
-            url: `https://www.fantasypros.com/${today.getFullYear()}/${today.getMonth() + 1}/fantasy-football-rankings-dynasty-trade-value-chart-${today.toLocaleString('default', { month: 'long' }).toLowerCase()}-${today.getFullYear()}-update/`,
-            icon: "assessment",
-            premium: false,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "DynastyProcess Trade Calculator",
-            url: "https://apps.dynastyprocess.com/calculator/",
-            icon: "calculate",
-            premium: false,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "Dynasty League Football",
-            url: "https://dynastyleaguefootball.com/",
-            icon: "insights",
-            premium: true,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "Reddit Dynasty Fantasy Football",
-            url: "https://www.reddit.com/r/DynastyFF/",
-            icon: "article",
-            premium: false,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "Reddit Fantasy Football",
-            url: "https://www.reddit.com/r/fantasyfootball/",
-            icon: "article",
-            premium: false,
-            separator: true,
-            redraftOnly: true,
-        },
-        {
-            name: "Dynasty 101 Trade Calculator",
-            url: "https://dynasty101.com/trade-calculator/",
-            icon: "calculate",
-            premium: false,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "Dynasty Nerds GM",
-            url: "https://gm.dynastynerds.com/",
-            icon: "insights",
-            premium: true,
-            separator: true,
-            dynastyOnly: true,
-        },
-        {
-            name: "Fantasy Football Trade Analyzer",
-            url: `https://fantasyfootballcalculator.com/trade-analyzer`,
-            icon: "calculate",
-            premium: false,
-            separator: true,
-            redraftOnly: true,
-        },
-        {
-            name: "Reddit FF Trade Analyzer",
-            url: "https://www.reddit.com/r/TradeAnalyzerFF/",
-            icon: "assessment",
-            premium: false,
-            separator: false,
-        },
-    ];
+	const today = new Date();
+	const resources = [
+		{
+		name: "FTN Fantasy",
+		url: "https://www.ftnfantasy.com/nfl",
+		icon: "insights",
+		premium: true,
+		separator: true,
+		},
+		{
+		name: "The Fantasy Footballers",
+		url: "https://www.thefantasyfootballers.com/",
+		icon: "podcasts",
+		premium: true,
+		separator: true,
+		},
+		{
+		name: "FantasyPros Dynasty Trade Value Chart",
+		url: `https://www.fantasypros.com/${today.getFullYear()}/${today.getMonth() + 1}/fantasy-football-rankings-dynasty-trade-value-chart-${today.toLocaleString('default', { month: 'long' }).toLowerCase()}-${today.getFullYear()}-update/`,
+		icon: "assessment",
+		premium: false,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "DynastyProcess Trade Calculator",
+		url: "https://apps.dynastyprocess.com/calculator/",
+		icon: "calculate",
+		premium: false,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "Dynasty League Football",
+		url: "https://dynastyleaguefootball.com/",
+		icon: "insights",
+		premium: true,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "Reddit Dynasty Fantasy Football",
+		url: "https://www.reddit.com/r/DynastyFF/",
+		icon: "article",
+		premium: false,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "Reddit Fantasy Football",
+		url: "https://www.reddit.com/r/fantasyfootball/",
+		icon: "article",
+		premium: false,
+		separator: true,
+		redraftOnly: true,
+		},
+		{
+		name: "Dynasty 101 Trade Calculator",
+		url: "https://dynasty101.com/trade-calculator/",
+		icon: "calculate",
+		premium: false,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "Dynasty Nerds GM",
+		url: "https://gm.dynastynerds.com/",
+		icon: "insights",
+		premium: true,
+		separator: true,
+		dynastyOnly: true,
+		},
+		{
+		name: "Fantasy Football Trade Analyzer",
+		url: `https://fantasyfootballcalculator.com/trade-analyzer`,
+		icon: "calculate",
+		premium: false,
+		separator: true,
+		redraftOnly: true,
+		},
+		{
+		name: "Reddit FF Trade Analyzer",
+		url: "https://www.reddit.com/r/TradeAnalyzerFF/",
+		icon: "assessment",
+		premium: false,
+		separator: false,
+		},
+		{
+		name: "Reddit FF Trade Analyzer",
+		url: "https://www.reddit.com/r/TradeAnalyzerFF/",
+		icon: "assessment",
+		premium: false,
+		separator: false,
+		},
+		{
+		name: "Reddit FF Trade Analyzer",
+		url: "https://www.reddit.com/r/TradeAnalyzerFF/",
+		icon: "assessment",
+		premium: false,
+		separator: false,
+		},
+		{
+		name: "Keep Trade Cut",
+		url: "https://keeptradecut.com/trade-calculator",
+		icon: "assessment",
+		premium: false,
+		separator: false,
+		},
+	];
 </script>
 
 <style>
