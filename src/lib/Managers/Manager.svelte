49cbf2f554f32243dc22dc3b6fd6e2333d23d58a
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 14:41:53 -0400

    testing rival changes

--- src/lib/Managers/Manager.svelte
+++ src/lib/Managers/Manager.svelte
@@ -64,161 +64,325 @@
 </script>
 
 <style>
-    .managerContainer {
-        width: 100%;
-        margin: 2em 0 5em;
-    }
+	.managerContainer {
+	width: 100%;
+	margin: 2em 0 5em;
+	}
 
-    .managerConstrained {
-        width: 97%;
-        max-width: 800px;
-        margin: 0 auto 4em;
-    }
+	.managerConstrained {
+	width: 97%;
+	max-width: 800px;
+	margin: 0 auto 4em;
+	}
 
-    .managerPhoto {
-        display: block;
-        border-radius: 100%;
-        width: 70%;
-        max-width: 200px;
-        height: auto;
-        margin: 5em auto 1em;
-        box-shadow: 0 0 8px 4px #aaa;
-    }
+	.managerPhoto {
+	display: block;
+	border-radius: 100%;
+	width: 70%;
+	max-width: 200px;
+	height: auto;
+	margin: 5em auto 1em;
+	box-shadow: 0 0 8px 4px #aaa;
+	}
 
-    h2 {
-        text-align: center;
-        font-size: 2.8em;
-        margin: 1em 0 0em;
-        line-height: 1em;
-    }
+	h2 {
+	text-align: center;
+	font-size: 2.8em;
+	margin: 1em 0 0em;
+	line-height: 1em;
+	}
 
-    h3 {
-        text-align: center;
-        font-size: 1.5em;
-        margin: 1.5em 0 0.5em;
-        font-weight: 200;
-    }
+	h3 {
+	text-align: center;
+	font-size: 1.5em;
+	margin: 1.5em 0 0.5em;
+	font-weight: 200;
+	}
 
-    .basicInfo {
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        height: 24px;
-        margin: 2em 0;
-    }
+	.basicInfo {
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	height: 24px;
+	margin: 2em 0;
+	}
 
-    .basicInfo span {
-        color: #888;
-        font-size: 0.9em;
-    }
+	.basicInfo span {
+	color: #888;
+	font-size: 0.9em;
+	}
 
-    .infoChild {
-        font-style: italic;
-    }
+	.infoChild {
+	font-style: italic;
+	}
 
-    .infoContact {
-        height: 20px;
-        vertical-align: middle;
-        padding-left: 1em;
-    }
+	.infoContact {
+	height: 20px;
+	vertical-align: middle;
+	padding-left: 1em;
+	}
 
-    .infoTeam {
-        height: 48px;
-    }
+	.infoTeam {
+	height: 48px;
+	}
 
-    .bio {
-        margin: 2em 1.5em 2em;
-        text-indent: 4em;
-    }
+	.bio {
+	margin: 2em 1.5em 2em;
+	text-indent: 4em;
+	}
 
-    .philosophy {
-        margin: 2em 1.5em 2em;
-        text-indent: 4em;
-    }
+	.philosophy {
+	margin: 2em 1.5em 2em;
+	text-indent: 4em;
+	}
 
-    .loading {
-        display: block;
-        width: 85%;
-        max-width: 500px;
-        margin: 80px auto;
-    }
+	.loading {
+	display: block;
+	width: 85%;
+	max-width: 500px;
+	margin: 80px auto;
+	}
 
-    .teamSub {
-        font-size: 0.4em;
-        line-height: 1em;
-        color: #666;
-    }
+	.teamSub {
+	font-size: 0.4em;
+	line-height: 1em;
+	color: #666;
+	}
 
-    .managerNav {
-        margin: 4em 0 2em;
-        text-align: center;
-    }
+	.managerNav {
+	margin: 4em 0 2em;
+	text-align: center;
+	}
 
-    .upper {
-        margin-top: 0;
-    }
+	.upper {
+	margin-top: 0;
+	}
 
-    .commissionerBadge {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 25px;
-        width: 25px;
-        font-weight: 600;
-        border-radius: 15px;
-        background-color: var(--blueTwo);
-        border: 1px solid var(--blueOne);
-    }
+	.commissionerBadge {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 25px;
+	width: 25px;
+	font-weight: 600;
+	border-radius: 15px;
+	background-color: var(--blueTwo);
+	border: 1px solid var(--blueOne);
+	}
 
-    .commissionerBadge span {
-        font-style: normal;
-        color: #fff;
-    }
+	.commissionerBadge span {
+	font-style: normal;
+	color: #fff;
+	}
 
-    /* media queries */
+	/* media queries */
 
-    @media (max-width: 505px) {
-        :global(.selectionButtons span) {
-            font-size: 0.8em;
-        }
-    }
+	@media (max-width: 505px) {
+	:global(.selectionButtons span) {
+	font-size: 0.8em;
+	}
+	}
 
-    @media (max-width: 435px) {
-        :global(.selectionButtons span) {
-            line-height: 1.2em;
-            font-size: 0.8em;
-        }
-    }
+	@media (max-width: 435px) {
+	:global(.selectionButtons span) {
+	line-height: 1.2em;
+	font-size: 0.8em;
+	}
+	}
 
 	@media (max-width: 450px) {
 
-        .basicInfo {
-            height: 20px;
-        }
-
-        .basicInfo span {
-            font-size: 0.75em;
-        }
-
-        .infoTeam {
-            height: 30px;
-        }
+	.basicInfo {
+	height: 20px;
 	}
 
-    @media (max-width: 370px) {
+	.basicInfo span {
+	font-size: 0.75em;
+	}
 
-        .basicInfo {
-            height: 18px;
-        }
+	.infoTeam {
+	height: 30px;
+	}
+	}
 
-        .basicInfo span {
-            font-size: 0.6em;
-        }
+	@media (max-width: 370px) {
 
-        .infoTeam {
-            height: 24px;
-        }
-    }
+	.basicInfo {
+	height: 18px;
+	}
+
+	.basicInfo span {
+	font-size: 0.6em;
+	}
+
+	.infoTeam {
+	height: 24px;
+	}
+	}
+
+	.fantasyInfos {
+	display: flex;
+	justify-content: space-around;
+	align-items: flex-start;
+	flex-wrap: wrap;
+	padding: 0 0 2em;
+	margin: 3em 0 4em;
+	border-bottom: 1px solid var(--aaa);
+	border-top: 1px solid var(--aaa);
+	box-shadow: 0 0 8px 4px var(--ccc);
+	}
+
+	.infoSlot {
+	text-align: center;
+	margin: 2em 1em 0;
+	}
+
+	.infoIcon {
+	display: inline-flex;
+	height: 70px;
+	width: 70px;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100%;
+	border: 1px solid var(--ccc);
+	overflow: hidden;
+	background-color: var(--fff);
+	transition: box-shadow 0.4s;
+	}
+
+	.playerIcon {
+	align-items:flex-end;
+	}
+
+	.infoLabel {
+	font-size: 0.7em;
+	color: var(--blueOne);
+	font-weight: 700;
+	margin-bottom: 1em;
+	height: 30px;
+	width: 90px;
+	text-align: center;
+	line-height: 1.2em;
+	}
+
+	.infoAnswer {
+	font-size: 0.8em;
+	color: var(--g555);
+	margin-top: 1em;
+	width: 90px;
+	text-align: center;
+	line-height: 1.2em;
+	}
+
+	.tradingScale {
+	line-height: 70px;
+	font-size: 55px;
+	color: var(--blueOne);
+	}
+
+	.rookiesOrVets {
+	height: 65px;
+	vertical-align: middle;
+	}
+
+	.infoRival {
+	cursor: pointer;
+	}
+
+	.infoRival:hover .infoIcon {
+	box-shadow: 0 0 6px 4px var(--aaa);
+	border: 1px solid var(--aaa);
+	}
+
+	.rival {
+	height: 100%;
+	}
+
+	.rebuildOrWin {
+	height: 70px;
+	}
+
+	.valuePosition {
+	line-height: 70px;
+	font-size: 25px;
+	color: var(--fff);
+	}
+
+	.QB {
+	background-color: var(--QB);
+	}
+
+	.WR {
+	background-color: var(--WR);
+	}
+
+	.RB {
+	background-color: var(--RB);
+	}
+
+	.TE {
+	background-color: var(--TE);
+	}
+
+	.Picks {
+	background: #73b647;
+	}
+	.K {
+	background-color: var(--K);
+	}
+
+	.DEF {
+	background-color: var(--DEF);
+	}
+
+	.CB {
+	background-color: #ffcc7a;
+	}
+
+	.SS {
+	background-color: #b7a1db;
+	}
+
+	.FS {
+	background-color: #ebe7b3;
+	}
+
+	.DE {
+	background-color: #b1d0e9;
+	}
+
+	.DL {
+	background-color: #c392d3;
+	}
+
+	.LB {
+	background-color: #98c097;
+	}
+
+	.favoritePlayer {
+	height: 65px;
+	vertical-align: bottom;
+	}
+
+	/* media queries */
+
+	@media (max-width: 731px) {
+	.infoSlot {
+	margin: 2em 3em 0;
+	}
+	}
+
+	@media (max-width: 558px) {
+	.infoSlot {
+	margin: 2em 2em 0;
+	}
+	}
+
+	@media (max-width: 461px) {
+	.infoSlot {
+	margin: 2em 1em 0;
+	}
+	}
 </style>
 
 <div class="managerContainer">
@@ -352,5 +516,86 @@
             {/if}
         </Group>
     </div>
-
+	<div class="fantasyInfos">
+		<!-- Rookies or Vets (optional) -->
+		{#if viewManager.rookieOrVets}
+		<div class="infoSlot">
+			<div class="infoLabel">
+				Rookie or Vet Preference
+			</div>
+			<div class="infoIcon">
+				<img class="rookiesOrVets" src="/{viewManager.rookieOrVets}.png" alt="rookie or vet preference"/>
+			</div>
+			<div class="infoAnswer">
+				{viewManager.rookieOrVets}
+			</div>
+		</div>
+		{/if}
+		<!-- Favorite fantasy position (optional) -->
+		{#if viewManager.valuePosition}
+		<div class="infoSlot">
+			<div class="infoLabel">
+				Favorite Fantasy Asset
+			</div>
+			<div class="infoIcon {viewManager.valuePosition}">
+				<span class="valuePosition">{viewManager.valuePosition}</span>
+			</div>
+		</div>
+		{/if}
+		{#if viewManager.tradingScale}
+		<!-- Trading Scale -->
+		<div class="infoSlot">
+			<div class="infoLabel">
+				Desire to Trade
+			</div>
+			<div class="infoIcon">
+				<span class="tradingScale">{viewManager.tradingScale}</span>
+			</div>
+			<div class="infoAnswer">
+				{viewManager.tradingScale} out of 10
+			</div>
+		</div>
+		{/if}
+		<!-- Favorite player (optioonal) -->
+		{#if viewManager.favoritePlayer}
+		<div class="infoSlot">
+			<div class="infoLabel">
+				Favorite Player
+			</div>
+			<div class="infoIcon playerIcon">
+				<img class="favoritePlayer" src="https://sleepercdn.com/content/nfl/players/{viewManager.favoritePlayer}.jpg" alt="favorite player"/>
+			</div>
+			<div class="infoAnswer">
+				{players[viewManager.favoritePlayer].fn} {players[viewManager.favoritePlayer].ln}
+			</div>
+		</div>
+		{/if}
+		<!-- Rebuild Mod (optional) -->
+		{#if viewManager.mode}
+		<div class="infoSlot">
+			<div class="infoLabel">
+				Win Now or Rebuild?
+			</div>
+			<div class="infoIcon">
+				<img class="rebuildOrWin" src="/{viewManager.mode.replace(' ', '%20')}.png" alt="win now or rebuild"/>
+			</div>
+			<div class="infoAnswer">
+				{viewManager.mode}
+			</div>
+		</div>
+		{/if}
+		<!-- Rival -->
+		<div class="infoSlot infoRival" on:click=""{() =>
+			gotoRival(viewManager.rival.link)}>
+			<div class="infoLabel">
+				Rival
+			</div>
+			<div class="infoIcon">
+				<img class="rival" src="{viewManager.rival.image}" alt="rival"/>
+			</div>
+			<div class="infoAnswer">
+				{viewManager.rival.name}
+			</div>
+		</div>
+	</div>
 </div>
