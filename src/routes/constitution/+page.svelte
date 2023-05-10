082a96887942f74eb64c2d7c826f7243e1d7c1a6
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 12:03:34 -0400

    undo

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -15,105 +15,84 @@
 </script>
 
 <style>
-	/*Floating Back-To-Top Button*/
-	#myBtn {
-	    position: fixed;
-	    bottom: 10px;
-	    float: right;
-	    right: 18.5%;
-	    left: 77.25%;
-	    max-width: 30px;
-	    width: 100%;
-	    font-size: 12px;
-	    border-color: rgba(85, 85, 85, 0.2);
-	    background-color: rgb(100,100,100);
-	    padding: .5px;
-	    border-radius: 4px;
+    .constitution {
+        position: relative;
+        z-index: 1;
+        width: 92%;
+        max-width: 800px;
+        margin: 8em auto 10em;
+    }
 
-	}
+    h1 {
+        font-size: 2em;
+        line-height: 1.2em;
+        text-align: center;
+        margin: 2em 0 1.5em;
+    }
 
-	/*On Hover Color Change*/
-	#myBtn:hover {
-	    background-color: #7dbbf1;
-	}
-	.constitution {
-	position: relative;
-	z-index: 1;
-	width: 92%;
-	max-width: 800px;
-	margin: 8em auto 10em;
-	}
+    h2 {
+        font-size: 1.5em;
+        line-height: 1.2em;
+    }
 
-	h1 {
-	font-size: 2em;
-	line-height: 1.2em;
-	text-align: center;
-	margin: 2em 0 1.5em;
-	}
+    h3 {
+        text-decoration: underline;
+        font-size: 1.3em;
+        line-height: 1.2em;
+    }
 
-	h2 {
-	font-size: 1.5em;
-	line-height: 1.2em;
-	}
+    h4 {
+        text-decoration: underline;
+        margin-left: 2em;
+        font-size: 1.2em;
+        line-height: 1.2em;
+    }
 
-	h3 {
-	text-decoration: underline;
-	font-size: 1.3em;
-	line-height: 1.2em;
-	}
+    h5 {
+        margin-left: 6em;
+        font-size: 0.8em;
+        line-height: 1.1em;
+    }
 
-	h4 {
-	text-decoration: underline;
-	margin-left: 2em;
-	font-size: 1.2em;
-	line-height: 1.2em;
-	}
+    .subBlock {
+        margin-left: 2.4em;
+    }
 
-	h5 {
-	margin-left: 6em;
-	font-size: 0.8em;
-	line-height: 1.1em;
-	}
+    .sectionHeading {
+        margin: 4em 0 1.5em;
+    }
 
-	.subBlock {
-	margin-left: 2.4em;
-	}
+    .subSectionHeading {
+        margin: 1.5em 0 1.5em;
+    }
 
-	.sectionHeading {
-	margin: 4em 0 1.5em;
-	}
+    .underscore {
+        text-decoration: underline;
+    }
 
-	.subSectionHeading {
-	margin: 1.5em 0 1.5em;
-	}
+    .right {
+        text-align: right;
+    }
 
-	.underscore {
-	text-decoration: underline;
-	}
+    .positionMaximums td {
+        min-width: 3em;
+    }
 
-	.right {
-	text-align: right;
-	}
+    .noUnderscore {
+        text-decoration: none;
+    }
 
-	.positionMaximums td {
-	min-width: 3em;
-	}
+    .clickable {
+        cursor: pointer;
+    }
 
-	.noUnderscore {
-	text-decoration: none;
-	}
+    .clickable:hover {
+        color: #00316b;
+    }
 
-	.clickable {
-	cursor: pointer;
-	}
-
-	.clickable:hover {
-	color: #00316b;
-	}
-
-	p {
-	color: #777;
-	}
+    p {
+        color: #777;
+    }
 </style>
 
 <div class="constitution">
@@ -320,9 +299,5 @@
 
     <h3 bind:this={sevenThree}>7.3 Raising Dues</h3>
     <p>A ⅔ majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buying, finding a co-manager to split the cost is recommended.</p>
-
-	<button id="myBtn">
-		<a href="#top" style="color: white">Top</a>
-	</button>
 
 </div>
