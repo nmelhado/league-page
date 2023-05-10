0b57abc736c54332d9b8d0ff3fe778fe1d635166
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 16:50:55 -0400

    test

--- src/lib/Managers/Manager.svelte
+++ src/lib/Managers/Manager.svelte
@@ -299,7 +299,7 @@
 
     {#if !loading}
         <!-- Favorite player -->
-        <ManagerFantasyInfo {viewManager} {players} />
+        <ManagerFantasyInfo {viewManager} {players} {leagueTeamManagers}/>
     {/if}
 
     <ManagerAwards {leagueTeamManagers} tookOver={viewManager.tookOver} {awards} {records} {rosterID} managerID={viewManager.managerID} />
