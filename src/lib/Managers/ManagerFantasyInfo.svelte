36ff6d382259039f113b8967d1108e50a633450b
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 16:02:44 -0400

    test

--- src/lib/Managers/ManagerFantasyInfo.svelte
+++ src/lib/Managers/ManagerFantasyInfo.svelte
@@ -1,15 +1,18 @@
 <script>
-    import { goto } from "$app/navigation";
+	import { goto } from "$app/navigation";
+	import { getAvatarFromTeamManagers } from '$lib/utils/helperFunctions/universalFunctions';
 
 
-    export let viewManager, players;
+	export let viewManager, players, leagueTeamManagers2;
 
-    const gotoRival = (rival) => {
-        if(!rival) {
-            goto(`/managers`);
-        }
-        goto(`/manager?manager=${rival}`);
-    }
+	$: ({rosterID, year} = viewManager.managerID ? getRosterIDFromManagerID(leagueTeamManagers, viewManager.managerID) : {rosterID: viewManager.roster, year: null});
+	
+	const gotoRival = (rival) => {
+	if(!rival) {
+	goto(`/managers`);
+	}
+	goto(`/manager?manager=${rival}`);
+	}
 </script>
 
 <style>
