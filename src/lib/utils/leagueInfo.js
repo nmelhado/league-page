2a5b1c325b6b73c89c12b3455b142c6231a27f52
Patrick Mann <patrick.mann8@gmail.com>
2023-05-10 17:17:39 -0400

    fix

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -58,7 +58,7 @@ export const managers = [
         "rival":
         {
             name: "The Don", // Can be anything (usually your rival's name)
-            link: 6, // manager array number within this array, or null to link back to all managers page
+            link: 7, // manager array number within this array, or null to link back to all managers page
             image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         "favoritePlayer": 4984, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
@@ -178,7 +178,7 @@ export const managers = [
         "rival":
         {
             name: "Don's Daddy", // Can be anything (usually your rival's name)
-            link: 1, // manager array number within this array, or null to link back to all managers page
+            link: 2, // manager array number within this array, or null to link back to all managers page
             image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         "favoritePlayer": 4984, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
