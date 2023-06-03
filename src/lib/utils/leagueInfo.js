10623d80691a11d2cf1824affa5dc434a8d5c722
DawgHouse73 (SOD) <133917715+dawghouse73@users.noreply.github.com>
2023-06-02 20:10:13 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -41,7 +41,7 @@ export const managers = [
        "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
     //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
        "philosophy": "When you're in, you're ALL in!",
-    //   "tradingScale": 10, // 1 - 10
+       "tradingScale": 8, // 1 - 10
        "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     // },
     // {
