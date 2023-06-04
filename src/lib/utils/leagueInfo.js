703dc4a2dc4c78062b43aac3fe88eccc9c2501e1
DawgHouse73 (SOD) <133917715+dawghouse73@users.noreply.github.com>
2023-06-04 12:17:56 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,8 +7,16 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to the Shulaces Out Dan Fantasy Football League site.</p>
-  <p>Founded: 2023</p>
+  <h1><p>Welcome to the Shulaces Out Dan Fantasy Football League site.</p></h1>
+  <b><p>Founded: 2023</p></b>
+  <p></p>
+  <p>The <b>Shulaces Out Dan</b> dynasty league arose from the ongoing comments and thoughts and chats from multiple keeper leagues that people were curious about the Dynasty front, sometimes stemming from frustration of having to give up a keeper due to the year limitation or having too many quality players and being forced to lose a high caliber player, sometimes for nothing in return.</p>
+  <p></p>
+  <p>Therein arrives the idea of kickstarting a dynasty league - combining managers from multiple leagues who've shown engagement, both on the social front and the league transaction perspective. Our league members are from all over the world, spanning a -8GMT to a +8GMT time zone variance.</p>
+  <p><img src="../static/worldmapofmanagers.png</p>
+  <p></p>
+  <p>Our history will be made in the coming years. But if history has taught us one thing, it's this:</p>
+  <h3><p>LACES OUT DAN!</p></h3>
 `;
 
 /*   STEP 3   */
