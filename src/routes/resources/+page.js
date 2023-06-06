ded8ac7f01676c7c8c248d3342ab755d277dcebf
DawgHouse73 (SOD) <133917715+dawghouse73@users.noreply.github.com>
2023-06-05 23:07:08 -0700

    Update +page.js

--- src/routes/resources/+page.js
+++ src/routes/resources/+page.js
@@ -1,9 +1,16 @@
 import { getNews } from '$lib/utils/helper';
 
+// Added by JCH - Jun 5, 2023
+import {getLeagueMatchups} from '$lib/utils/helperFunctions/leagueMatchups'
+// End Edit
+
 export async function load({fetch}) {
     const articlesData = getNews(fetch);
 
     return {
         articlesData
     };
+}
+export {
+    getLeagueMatchups,
 }
