ea090301302f762b979cc24be34a4562fa0fb158
DawgHouse73 (SOD) <133917715+dawghouse73@users.noreply.github.com>
2023-06-09 20:46:46 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -160,7 +160,7 @@ export const managers = [
       "rival":  {
         name: "Everyone", 
         link: null, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/everyone.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "/managers/everyone.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
                 },
        "favoritePlayer": 123, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "WR", // (optional) 
