76c08b508646b3141c18380e7622d41ac7a382b2
Nicholas Melhado <n.melhado@gmail.com>
2023-06-25 12:45:48 -0400

    remove league ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "918537087509577728"; // your league ID
+export const leagueID = "your_league_id"; // your league ID
 export const leagueName = "your_league_name"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
