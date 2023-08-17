5825c9f6e68e06746a3013142592cb16bc3a4170
lzente <142539342+lzente@users.noreply.github.com>
2023-08-17 19:44:49 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -34,7 +34,7 @@ export const managers = [
       "favoriteTeam": "car", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
+        name: "Boles", // Can be anything (usually your rival's name)
         link:1 , // manager array number within this array, or null to link back to all managers page
         image: "/managers/boles.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
