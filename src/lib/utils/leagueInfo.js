f79c49bf64a144fc00717acb9f9ae77996284bf7
lzente <142539342+lzente@users.noreply.github.com>
2023-08-17 19:56:53 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -58,7 +58,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
-        link:7 , // manager array number within this array, or null to link back to all managers page
+        link:6 , // manager array number within this array, or null to link back to all managers page
         image: "/managers/john.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer":1181 , // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
@@ -150,7 +150,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Jush", // Can be anything (usually your rival's name)
-        link:8 , // manager array number within this array, or null to link back to all managers page
+        link:7 , // manager array number within this array, or null to link back to all managers page
         image: "/managers/josh.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer":5870  , // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
