65e34afb717e5776d41d19a8aaf175925f19ba57
lzente <142539342+lzente@users.noreply.github.com>
2023-08-17 20:18:14 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -196,7 +196,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Boris", // Can be anything (usually your rival's name)
-        link:8 , // manager array number within this array, or null to link back to all managers page
+        link:7 , // manager array number within this array, or null to link back to all managers page
         image: "/managers/forest.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer":5870  , // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
