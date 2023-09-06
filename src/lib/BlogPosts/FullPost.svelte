ccea0805180462745d6d8b00fe8b8e8b55e3cced
Nicholas Melhado <n.melhado@gmail.com>
2023-09-06 14:49:16 -0400

    update blog handling

--- src/lib/BlogPosts/FullPost.svelte
+++ src/lib/BlogPosts/FullPost.svelte
@@ -1,15 +1,41 @@
 <script>
+	import LinearProgress from '@smui/linear-progress';
     import { parseDate, getAuthor, getAvatar, generateParagraph } from "$lib/utils/helper";
     import { onMount } from "svelte";
-    import { fly } from "svelte/transition";
     import Comments from "./Comments.svelte";
 
-    export let leagueTeamManagers, post, createdAt, id = null, direction = 1, home = false;
+    export let leagueTeamManagersData, postsData, postID;
+
+    let createdAt, id;
+
+    let safePost = false;
+    let loading = true;
+    let title, body, type, author;
 
     let loadingComments = true;
     let total, comments;
 
     onMount(async()=> {
+        const post = postsData.posts.filter(p => p.sys.id === postID)[0];
+        id = post.sys.id;
+
+        if(post != null) {
+            createdAt = post.sys.createdAt;
+            ({title, body, type, author} = post.fields);
+            if(!title) {
+                console.error('Invalid post: No title provided');
+            } else if(!body) {
+                console.error(`Invalid post (${title}): No body provided`)
+            } else if(!type) {
+                console.error(`Invalid post (${title}): No type provided`)
+            } else if(!author) {
+                console.error(`Invalid post (${title}): No author provided`)
+            } else {
+                safePost = true;
+            }
+        }
+        loading = false;
+
         const res = await fetch(`/api/getBlogComments/${id}`, {compress: true});
         const commentsData = await res.json();
 
@@ -18,29 +44,7 @@
         loadingComments = false;
     });
 
-    let safePost = false;
-    let title, body, type, author;
-
-    if(post != null) {
-        ({title, body, type, author} = post);
-        if(!title) {
-            console.error('Invalid post: No title provided');
-        } else if(!body) {
-            console.error(`Invalid post (${title}): No body provided`)
-        } else if(!type) {
-            console.error(`Invalid post (${title}): No type provided`)
-        } else if(!author) {
-            console.error(`Invalid post (${title}): No author provided`)
-        } else {
-            safePost = true;
-        }
-    }
-
     const duration = 300;
-
-    let e;
-
-    $: isOverflown = e ? e.scrollHeight > e.clientHeight : false;
 </script>
 
 <style>
@@ -59,83 +63,82 @@
         margin: 0;
     }
 
-    .button {
+    .authorAndDate a {
         background-color: #00316b;
+        color: #fff;
         border-radius: 1em;
         text-decoration: none;
         font-size: 0.8em;
         padding: 0.5em 1em;
         margin-right: 1em;
-        color: var(--fff);
     }
 
-    .large {
-        font-size: 1em;
-        padding: 0.5em 1em;
+    .authorAndDate a:hover {
         background-color: #0082c3;
-    }
-
-    .button:hover {
-        background-color: #0082c3;
-    }
-
-    .large:hover {
-        background-color: #00316b;
     }
 
     :global(.body blockquote) {
+        margin: 0.1em 0;
         border-left: 3px solid rgb(231, 235, 238);
         margin: 1em 2em;
         padding-left: 0.875em;
     }
 
     :global(.body .heading-1) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.9em;
         text-align: center;
     }
 
     :global(.body .heading-2) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.8em;
         text-align: center;
     }
 
     :global(.body .heading-3) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.7em;
         text-align: center;
     }
 
     :global(.body .heading-4) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.6em;
         text-align: center;
     }
 
     :global(.body .heading-5) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.5em;
         text-align: center;
     }
 
     :global(.body .heading-6) {
-        padding: 0.4em 2em;
+        margin: 0.4em 0;
+        padding: 0 2em;
         font-size: 1.4em;
         text-align: center;
     }
 
     :global(.body .bodyParagraph) {
-        padding: 1em 2em;
-        margin: 0;
+        margin: 1em 0;
+        padding: 0 2em;
     }
 
     :global(.body ul) {
-        padding: 1em 2em 1em 4em;
+        margin: 1em 0;
+        padding: 0 2em 0 4em;
     }
 
     :global(.body ol) {
-        padding: 1em 2em 1em 4em;
+        margin: 1em 0;
+        padding: 0 2em 0 4em;
     }
 
     :global(.body .bodyParagraph a) {
@@ -143,7 +146,8 @@
     }
 
     :global(.body .blogImg) {
-        padding: 1em 2em;
+        margin: 1em 0;
+        padding: 0 2em;
         display: flex;
         justify-content: center;
     }
@@ -197,33 +201,15 @@
         padding: 0 2em;
     }
 
-    .body {
-        position: relative;
-        max-height: 9em;
-        overflow: hidden;
+    :global(.authorAndDate a) {
+        color: var(--g999);
     }
 
-    .fade {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-    }
-
-    .fadeTop {
-        height: 2em;
-        width: 100%;
-        background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255, 255, 255, 1));
-    }
-
-    .fadeBottom {
-        height: 1em;
-        width: 100%;
-        background-color: white;
-    }
-
-    .viewFull {
-        padding: 0.2em 2em 1em;
+    .loading {
+        display: block;
+        width: 85%;
+        max-width: 500px;
+        margin: 80px auto;
     }
 </style>
 
@@ -232,35 +218,37 @@
     (bug https://github.com/nmelhado/league-page/issues/141)
     This if check makes blog enablement more flexible
 -->
-{#if safePost}
-    {#key id}
-        <div in:fly={{delay: duration, duration: duration, x: 150 * direction}} out:fly={{delay: 0, duration: duration, x: -150 * direction}} class="post">
-            <h3>{title}</h3>
 
-            <div class="body" bind:this={e} style="padding-bottom: {isOverflown ? '3em' : '0'}">
-                {#each body.content as paragraph}
-                    {@html generateParagraph(paragraph)}
-                {/each}
-                {#if isOverflown}
-                    <div class="fade">
-                        <div class="fadeTop" />
-                        <div class="fadeBottom" />
-                    </div>
-                {/if}
-            </div>
-            
-            <div class="viewFull">
-                <a class="button large" href="/blog/{id}">View Full Post</a>
-            </div>
+{#if loading}
+    <!-- promise is pending -->
+    <div class="loading">
+        <p>Loading Blog Post...</p>
+        <LinearProgress indeterminate />
+    </div>
+{:else if safePost}
+    <div class="post">
+        <h3>{title}</h3>
 
-            <hr class="divider" />
-
-            <div class="authorAndDate">
-                <a class="button" href="/blog?filter={type}&page=1">{type}</a>
-                <img alt="author avatar" class="teamAvatar" src="{getAvatar(leagueTeamManagers, author)}" />
-                <span class="author">{@html getAuthor(leagueTeamManagers, author)} - </span>
-                <span class="date"><i>{parseDate(createdAt)}</i></span>
-            </div>
+        <div class="body">
+            {#each body.content as paragraph}
+                {@html generateParagraph(paragraph)}
+            {/each}
         </div>
-    {/key}
+
+        <hr class="divider" />
+
+        <div class="authorAndDate">
+            <a href="/blog?filter={type}&page=1">{type}</a>
+            <img alt="author avatar" class="teamAvatar" src="{getAvatar(leagueTeamManagersData, author)}" />
+            <span class="author">{@html getAuthor(leagueTeamManagersData, author)} - </span>
+            <span class="date"><i>{parseDate(createdAt)}</i></span>
+        </div>
+
+        <!-- display comments -->
+        {#if !loadingComments}
+            <hr class="divider commentDivider" />
+            <Comments leagueTeamManagers={leagueTeamManagersData} {comments} {total} postID={id} />
+        {/if}
+
+    </div>
 {/if}
