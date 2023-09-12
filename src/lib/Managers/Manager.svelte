d681f3e774d8978f2cda3f0747c709d2f29acd8c
Nikhil Katti <103154211+n-katti@users.noreply.github.com>
2023-09-12 00:47:49 -0400

    Update Manager.svelte

--- src/lib/Managers/Manager.svelte
+++ src/lib/Managers/Manager.svelte
@@ -128,13 +128,11 @@
     }
 
     .bio {
-	text-align: center
         margin: 2em 1.5em 2em;
         text-indent: 4em;
     }
 
     .philosophy {
-	text-align: center
         margin: 2em 1.5em 2em;
         text-indent: 4em;
     }
