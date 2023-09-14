acc112fbef8448fc0db7c40ca51099c45f8fccab
Nikhil Katti <103154211+n-katti@users.noreply.github.com>
2023-09-13 23:47:08 -0400

    changed blake

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -48,7 +48,7 @@ export const managers = [
     },
     {
       // "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID": "867629698715406336",
+      "managerID": "992181047900819456",
       "name": "Blake",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "The Clouds", // (optional)
