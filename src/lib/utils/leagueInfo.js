0c70da69871d16aeb93cedfde7017456f269ca82
sethtobrien <140202779+sethtobrien@users.noreply.github.com>
2023-09-15 13:20:44 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,10 +28,13 @@ export const managers = [
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 76748153545965568"
       "name": "Seth",
+      "tookOver": null,
       "location": "Chattanooga", // (optional)
       "bio": "There are only two certainties in V3... This owner spamming trades and exploiting loopholes.",
       "photo": "/managers/Seth.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Trade for studs or don't trade at all",
@@ -42,10 +45,13 @@ export const managers = [
       "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 86713839919710208"
       "name": "Joe",
+      "tookOver": null,
       "location": "Durham", // (optional)
       "bio": "L33t c0d3r. Early bloomer. Zero RB enthusiast.",
       "photo": "/managers/Joe.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "TE", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Outsmarting the league is what I do.",
@@ -56,10 +62,13 @@ export const managers = [
       "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 461971199233093632"
       "name": "Andy",
+      "tookOver": null,
       "location": "Tampa", // (optional)
       "bio": "Yankees stan. Known for trading studs to Seth and causing V3 chaos.",
       "photo": "/managers/Andy.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Player performance is more important than ADP.",
@@ -70,10 +79,13 @@ export const managers = [
       "roster": 4,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 506532552412807168"
       "name": "Brandon",
+      "tookOver": null,
       "location": "Denver", // (optional)
       "bio": "Always in the ship, but can't close the deal.",
       "photo": "/managers/Brandon.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Advanced stats nerd",
@@ -84,10 +96,13 @@ export const managers = [
       "roster": 5,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 599323969325170688"
       "name": "Patrick",
+      "tookOver": null,
       "location": "Oahu", // (optional)
       "bio": "As commissioner of this great league, you will bow to my greatness",
       "photo": "/managers/Patrick.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "bal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Rules are life",
@@ -98,10 +113,13 @@ export const managers = [
       "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 627356848126754816"
       "name": "Carter",
+      "tookOver": null,
       "location": "Boston", // (optional)
       "bio": "How did Patrick trick me into joining this league?",
       "photo": "/managers/Carter.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Smart ownership wins championships",
@@ -112,10 +130,13 @@ export const managers = [
       "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 627357544234430464"
       "name": "Weston",
+      "tookOver": null,
       "location": "Seattle", // (optional)
       "bio": "4x V3 Kang",
       "photo": "/managers/Weston.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Alpha over everything",
@@ -126,10 +147,13 @@ export const managers = [
       "roster": 8,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 627358262001479680"
       "name": "Tyler",
+      "tookOver": null,
       "location": "Apex", // (optional)
       "bio": "iRacing > Fantasy Football",
       "photo": "/managers/Tyler.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "car", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Minimal effort. Minimal results.",
@@ -140,10 +164,13 @@ export const managers = [
       "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 627359864607277056"
       "name": "Mike",
+      "tookOver": null,
       "location": "Raleigh", // (optional)
       "bio": "Trish loves that V3 keeps me up to date with all the modern lingo",
       "photo": "/managers/Mike.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Say you'll never draft a player again, then draft him.",
@@ -154,10 +181,13 @@ export const managers = [
       "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 627679130321874944"
       "name": "Justen",
+      "tookOver": null,
       "location": "Greenville", // (optional)
       "bio": "Extreme ownership is what is missing in this league",
       "photo": "/managers/Justen.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Fair and balanced. This is the way.",
@@ -168,10 +198,13 @@ export const managers = [
       "roster": 11,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 630882921305161728"
       "name": "Adam",
+      "tookOver": null,
       "location": "Denver", // (optional)
       "bio": "Gators. NY Giants. Denver Teams.",
       "photo": "/managers/Adam.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Don't trade with Seff",
@@ -182,10 +215,13 @@ export const managers = [
       "roster": 12,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : 631191024458559488"
       "name": "Dave",
+      "tookOver": null,
       "location": "San Diego", // (optional)
       "bio": "I hate V3 but V3 needs me",
       "photo": "/managers/Dave.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": null,
       "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": null,
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Always find a way to win the doll",
