b882afc40d57f9a75a172123353690d143d68df8
Daniel Bernal <daniel@bernal.net>
2023-09-25 15:46:45 -0500

    2023

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "847326716543217664"; // your league ID
+export const leagueID = "995202585495711744"; // your league ID
 export const leagueName = "Balls Deep Football"; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
