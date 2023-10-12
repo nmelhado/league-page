33f8e4ecb80b1d60ae7051a9755efd49e1e04f12
matt-t22 <147672727+matt-t22@users.noreply.github.com>
2023-10-11 21:46:52 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,7 +25,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-    // {
+    {
     //   "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "846864033322762240",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
       "name": "Tits",
