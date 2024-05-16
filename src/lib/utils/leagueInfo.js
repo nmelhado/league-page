9480359b63225c46c60b6c65bcb42bf71d1ba78f
gsteuter <gsteuter@gmail.com>
2024-05-15 21:07:54 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,13 +1,13 @@
 /*   STEP 1   */
 export const leagueID = "1082729986064244736"; // your league ID
-export const leagueName = "The Dipstick Clique"; // your league name
+export const leagueName = "The Dipstick Clique™ EST.2018"; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to the official website of The Dipstick Clique™. EST.2018</p>
+  <p>Welcome to the official website of The Dipstick Clique!</p>
   <p>A league dedicated to highschool "friends" who may hate more people in the league than they actually like.</p>
   
 `;
