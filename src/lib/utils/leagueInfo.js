4898221839d0fb767a76ed9fff17dc2004656a2c
gsteuter <gsteuter@gmail.com>
2024-05-17 19:54:54 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -21,7 +21,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 // To omit an optional field, set it's value to null
 
-export const managers = [
+// export const managers = [
   //   {
   //     "managerID": "863606469411606528",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
   //     "name": "Champ/Swag King/Touchdown",
