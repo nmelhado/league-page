347e5e1a19876e7a604a8a77f4eecc0ab7e8a65f
gsteuter <gsteuter@gmail.com>
2024-05-17 19:39:06 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,7 +25,7 @@ export const managers = [
     {
       "managerID": "863606469411606528",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Champ/Swag King/Touchdown",
-      "tookOver": "2018", // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      "tookOver": "null", // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Tremont, OH", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
@@ -37,10 +37,10 @@ export const managers = [
         link: 6, // manager array number within this array, or null to link back to all managers page
         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy",
+      "favoritePlayer": , // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "valuePosition": "", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+      "rookieOrVets": "", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+      "philosophy": "Under 18 is fine with me!",
       "tradingScale": 10, // 1 - 10
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
