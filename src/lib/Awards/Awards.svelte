4479fbeb0978e6980971bc3f596fd6433fc01962
Nikhil <nikhilkatti@outlook.com>
2024-09-11 23:38:59 -0400

    final presleeper

--- src/lib/Awards/Awards.svelte
+++ src/lib/Awards/Awards.svelte
@@ -385,6 +385,76 @@
 			<span class="genLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: toilet})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, toilet)}</span>
 		</div>
 	{/if}
-</div>	
 
-<PreSleeperAwards />
+		<!-- Conditionally render Pre-Sleeper Awards if the year is 2022 -->
+	{#if year == 2022}
+		<!-- <PreSleeperAwards /> -->
+		<h3>Pre-Sleeper Awards</h3>
+
+		<h4>2021</h4>
+		<img src="./banner.png" class="banner" alt="The Champion's Cup" />
+	
+		<div id="podium">
+	
+			<!-- champs -->
+			<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 4, year)}" class="first champ clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 4})} alt="champion" />
+			
+			<span class="label firstLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 4})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 4)}</span>
+		</div>
+	
+		<div class="toiletParent">
+				
+				<img src="./toilet-banner.png" class="toilet-banner" alt="The Toilet Bowl" />
+	
+				<div class="toiletBowl">
+					<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 9, year)}" class="toiletWinner clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 9})} alt="toilet bowl winner" />
+					<img src="./toilet-bowl-2.png" class="toilet" alt="toilet bowl" />
+				</div>
+				<span class="genLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 9})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 9)}</span>
+		</div>
+	
+		<h4>2020</h4>
+		<img src="./banner.png" class="banner" alt="The Champion's Cup" />
+	
+		<div id="podium">
+	
+			<!-- champs -->
+			<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 1, year)}" class="first champ clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 1})} alt="champion" />
+			
+			<span class="label firstLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 1})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 1)}</span>
+		</div>
+	
+		<div class="toiletParent">
+				
+				<img src="./toilet-banner.png" class="toilet-banner" alt="The Toilet Bowl" />
+	
+				<div class="toiletBowl">
+					<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 3, year)}" class="toiletWinner clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 3})} alt="toilet bowl winner" />
+					<img src="./toilet-bowl-2.png" class="toilet" alt="toilet bowl" />
+				</div>
+				<span class="genLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 3})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 3)}</span>
+		</div>
+	
+		<h4>2019</h4>
+		<img src="./banner.png" class="banner" alt="The Champion's Cup" />
+	
+		<div id="podium">
+	
+			<!-- champs -->
+			<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 3, year)}" class="first champ clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 3})} alt="champion" />
+			
+			<span class="label firstLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 3})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 3)}</span>
+		</div>
+	
+		<div class="toiletParent">
+				
+				<img src="./toilet-banner.png" class="toilet-banner" alt="The Toilet Bowl" />
+	
+				<div class="toiletBowl">
+					<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 8, year)}" class="toiletWinner clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 8})} alt="toilet bowl winner" />
+					<img src="./toilet-bowl-2.png" class="toilet" alt="toilet bowl" />
+				</div>
+				<span class="genLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 8})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 8)}</span>
+		</div>
+	{/if}
+</div>	
