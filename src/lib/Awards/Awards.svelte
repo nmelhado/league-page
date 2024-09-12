ae16caccc1b34bf2b995511feedb8be9fda14c40
Nikhil <nikhilkatti@outlook.com>
2024-09-11 23:15:35 -0400

    trying something new

--- src/lib/Awards/Awards.svelte
+++ src/lib/Awards/Awards.svelte
@@ -384,4 +384,14 @@
 			<span class="genLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: toilet})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, toilet)}</span>
 		</div>
 	{/if}
-</div>
+
+	<img src="./banner.png" class="banner" alt="The Champion's Cup" />
+
+	<div id="podium">
+
+		<!-- champs -->
+		<img src="{getAvatarFromTeamManagers(leagueTeamManagers, 4, year)}" class="first champ clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 4})} alt="champion" />
+		
+		<span class="label firstLabel clickable" on:click={() => gotoManager({year, leagueTeamManagers, rosterID: 4})}>{@html getNestedTeamNamesFromTeamManagers(leagueTeamManagers, year, 4)}</span>
+	</div>
+
