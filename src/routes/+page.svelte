7c8cd8aee8248cfc7dd7df775dc8b838efe4d05a
j-preim <j.preimesberger@gmail.com>
2024-09-30 20:19:46 -0500

    league name size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -139,7 +139,7 @@
 <div id="home">
     <div id="main">
         <div class="text">
-            <h6>{leagueName}</h6>
+            <h4>{leagueName}</h4>
             <!-- homepageText contains the intro text for your league, this gets edited in /src/lib/utils/leagueInfo.js -->
             {@html homepageText }
             <!-- Most recent Blog Post (if enabled) -->
