e73358ab8c50d1f47f2df384b1aa2cab04e5291c
CptTobez <141162879+CptTobez@users.noreply.github.com>
2024-10-21 21:34:05 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "934307493323341824"; // your league ID
+export const leagueID = "1050016498774925312"; // your league ID
 export const leagueName = "Dynasty Warriors"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
