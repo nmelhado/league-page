b0f6ea60b17033fc58b08a09a9be9ec8e9854d03
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-04 18:50:33 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -30,7 +30,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "roster": 1, "844760551790858240" (look at the order of the power rankings graph)
+      "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Eddie F",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Manor, TX", // (optional)
