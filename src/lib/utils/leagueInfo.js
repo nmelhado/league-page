4aac06e11fd7eaf73845dbe43fd58669e6bb06a1
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-07 17:47:46 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to MFFLs (Manor Fantasy Football League) website, where competition and camaraderie go hand in hand!. Founded in 2014 by Eddie Fuentes I invited a tight-knit group of friends and family from there, we've built a 
+  <p>Welcome to MFFLs website, where competition and camaraderie go hand in hand!. Founded in 2014 by Eddie Fuentes I invited a tight-knit group of friends and family from there, we've built a 
   league where every season is a battle for glory. What started with nine teams in three divisions has grown into a fierce sixteen-team league with four divisions, using superflex and PPR scoring system, and a prize pool of 
   $1,000 up for grabs. Our core teams, The Comeback Kid, Raiders Fantasy Blue Ballers, SlickBears, 88Boyz11 and Texas Timeshifts have set the tone since day one, bringing intense rivalries and unforgettable matchups.
   A few years later the competition heated up with the addition of The People’s Champion, Bay Area Party Supplies, Vick2Times, and our latest players, Fristoe, Prime Time JG, CeeDees TDs and finally, Do It To Them and Loud and Stroud.
