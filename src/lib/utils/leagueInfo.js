b89f45bf5ed054bb6eee90d8ccba6c0b5c8c3467
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-08 10:05:15 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -93,7 +93,7 @@ export const managers = [
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Pflugerville TX", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/TF.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/TF3.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
