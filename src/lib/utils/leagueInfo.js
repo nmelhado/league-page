83ad23e3eb1144876f648471f4edbcf5ef1946c8
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-07 22:57:49 -0600

    Update leagueInfo.js_JDD

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -135,8 +135,8 @@ export const managers = [
       "roster": 5,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "John Diaz-Decaro",
       "location": "Los Angels California", // (optional)
-      "bio": "...",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "bio": "I am inevitable",
+      "photo": "/managers/JDD.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2021, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
