ec4b374c89e9796694826d2a9addcc0be89230bf
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-07 23:34:47 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -174,6 +174,7 @@ export const managers = [
       "tradingScale": 10, // 1 - 10
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
+        
   ]
   
   
