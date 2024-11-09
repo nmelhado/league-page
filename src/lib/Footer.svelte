1cd89460500f23d9d14e07609b7e80c78dbd8c55
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-08 22:13:37 -0600

    leagueinfo.js

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -130,5 +130,5 @@
 	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
 	<!-- You can remove the donation link (although any donations to help
 	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+	
 </footer>
