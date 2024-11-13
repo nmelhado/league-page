47b8a378257abb6354cf6115d2351dca5d767041
matt-t22 <147672727+matt-t22@users.noreply.github.com>
2024-11-12 20:21:40 -0600

    Update leagueInfo.js to 2024

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "930840153906679808"; // your league ID
+export const leagueID = "1073309575626993664"; // your league ID
 export const leagueName = "12 Angry Sigs"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
