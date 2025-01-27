a331193b9398fd69cbab134133da22ed0c170da1
tzward <tzward69@gmail.com>
2025-01-27 16:40:03 -0500

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,1 +1,2 @@
+npx create-next-app --example cms-contentful cms-contentful-app
 npx cross-env CONTENTFUL_SPACE_ID=wf13nkh7vltc CONTENTFUL_MANAGEMENT_TOKEN=CFPAT-HIYL4rKUV9x2fEuio-udzZo2dsO0nzAEJYsFm0vwM2c pnpm run setup
