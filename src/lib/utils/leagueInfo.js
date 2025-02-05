140a85230f238dbde467a4ac11e11e131a05728c
tzward <tzward69@gmail.com>
2025-02-05 17:36:41 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -240,7 +240,7 @@ export const managers = [
          link: 7, // manager array number within this array, or null to link back to all managers page
          image: "/managers/File 13.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
-       "favoritePlayer": 4805, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "favoritePlayer": 1362, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
        "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
        "philosophy": "Your fantasy team's philosophy",
