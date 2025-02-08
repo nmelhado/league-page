33cecb359afa70428ca2efed9218e226bb8b4a83
Nicholas Melhado <n.melhado@gmail.com>
2025-02-08 10:46:22 -0500

    add analytics (#344)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <!-- __layout.svelte -->
 <script>
 	import { Nav, Footer } from "$lib/components"
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
