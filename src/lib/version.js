cf2376851e0ccbbdf9b4e3a72ba39fbf261c240d
tzward <tzward69@gmail.com>
2025-02-09 19:22:15 -0500

    Update version.js

--- src/lib/version.js
+++ src/lib/version.js
@@ -5,8 +5,5 @@ available for your copy of League Page
 */
 
 // Keep in sync with package.json
-<<<<<<< HEAD
-export const version = "2.5.0";
-=======
 export const version = "2.3.5";
->>>>>>> 917b29edcd95a5d827ead73a4df45d1ae284e9e5
+
