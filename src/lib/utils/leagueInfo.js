648e269598b639c47ca42d9c29d166e89868e81e
tzward <tzward69@gmail.com>
2025-02-09 20:35:38 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -81,7 +81,7 @@ export const managers = [
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "East Rutherford, NJ", // (optional)
        "bio": "You know, Quasimodo predicted all of this.",
-       "photo": "/managers/tony 2.jpeg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/tony 2.png", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -113,7 +113,7 @@ export const managers = [
        "rival": {
          name: "Mr. S(o)p(r)ano", // Can be anything (usually your rival's name)
          link: 2, // manager array number within this array, or null to link back to all managers page
-         image: "/managers/tony 2.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         image: "/managers/tony 2.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
        "favoritePlayer": 2078, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
