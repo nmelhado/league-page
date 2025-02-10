7aef1d752683caa9159cbf90441eb14c34c949a6
tzward <tzward69@gmail.com>
2025-02-09 19:27:47 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <!-- __layout.svelte -->
 <script>
 	import { Nav, Footer } from "$lib/components"
-    import { dev } from '$app/environment';
-    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+  #  import { dev } from '$app/environment';
+  #  import { injectAnalytics } from '@vercel/analytics/sveltekit';
  
-    injectAnalytics({ mode: dev ? 'development' : 'production' });
+  #  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
