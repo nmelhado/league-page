55bb19ad4a1132bfd3cd34df0209f3ef6b35aa33
Nicholas Melhado <n.melhado@gmail.com>
2025-03-02 06:46:48 -0500

    update league ID for 2025 season

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1050069781250048000"; // your league ID
+export const leagueID = "1201891419585785856"; // your league ID
 export const leagueName = "The Legends League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
