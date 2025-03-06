c5833686d5120b18c15623c743eb49649e696274
Msefyousef <Msefyousef@gmail.com>
2025-03-06 14:16:24 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1183104551146070016"; // your league ID
-export const leagueName = "No Friends in Fantasy Fooball League"; // your league name
+export const leagueName = "No Friends in Fantasy Football League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
