f037ccc8d9d4a00779257418ae3ac6d0bf45f997
Msefyousef <Msefyousef@gmail.com>
2025-03-15 23:58:31 -0400

    Update index.svelte

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -23,7 +23,7 @@
 </script>
 
 <svelte:head>
-	<title>{!page.url.pathname[1] ? 'Home' : page.url.pathname[1].toUpperCase() + page.url.pathname.slice(2)} | League Page</title>
+	<title>{!page.url.pathname[1] ? 'Home' : page.url.pathname[1].toUpperCase() + page.url.pathname.slice(2)} | NFIF</title>
 </svelte:head>
 
 <style>
