0e7d92936877ddd2b7ef725e791d5570a1c5cebd
Pnysch97 <99479390+Pnysch97@users.noreply.github.com>
2025-06-25 11:15:12 -0400

    style: refresh layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,9 +57,11 @@
         min-width: 470px;
         max-width: 470px;
         min-height: 100%;
-		background-color: var(--ebebeb);
-        border-left: var(--eee);
-		box-shadow: inset 0px 3px 3px -2px rgb(0 0 0 / 40%), inset 0px 3px 4px 0px rgb(0 0 0 / 28%), inset 0px 1px 8px 0px rgb(0 0 0 / 24%);
+        padding: 1rem;
+        background: linear-gradient(to bottom right, var(--fff), var(--f8f8f8));
+        border: 1px solid var(--eee);
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
     }
 
     @media (max-width: 950px) {
